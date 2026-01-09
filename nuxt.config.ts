5ed03d9c16c559481d63bfa4110176854c6078f9
Marco Bruno <marco.bruno.dev@proton.me>
2026-01-09 19:58:03 +0100

    meta: change metadata to banana server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 
 const sw = process.env.SW === "true";
 
-const title = "5Stack | Counter-Strike Management System";
+const title = "Banana Server";
 const description =
   "A Comprehensive Panel for Managing Servers, Matches, and Tournaments";
 
 // TODO - i tired to get SSO to work but it wont
-const url = `https://5stack.gg`;
+const url = `https://banana-server.xyz`;
 
 export default defineNuxtConfig({
   ssr: false,
@@ -27,13 +27,13 @@ export default defineNuxtConfig({
         { name: "robots", content: "index, follow" },
         { name: "title", content: title },
         { name: "description", content: description },
-        { name: "site_name", content: "5Stack" },
+        { name: "site_name", content: "Banana Server" },
 
         { property: "og:locale", content: "en" },
         { property: "og:type", content: "website" },
 
         { property: "og:title", content: title },
-        { property: "og:site_name", content: "5Stack" },
+        { property: "og:site_name", content: "Banana Server" },
 
         { property: "og:url", content: url },
         { property: "og:image", content: `${url}/_ipx/_/favicon/512.png` },
@@ -174,8 +174,8 @@ export default defineNuxtConfig({
       enabled: true,
     },
     manifest: {
-      name: "5stack",
-      short_name: "5stack",
+      name: "Banana Server",
+      short_name: "Banana Server",
       icons: [
         {
           src: "/favicon/64.png",
